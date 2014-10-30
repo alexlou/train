03174e836fed2bfed3f89ed79a8fdd07f6cd7f12
alexlou <chunwei.lou@gmail.com>
2014-10-30 22:57:48 +0800

    add hashcode and equals

--- datastruct/main/java/lou/alex/train/datastruct/LinkedListNode.java
+++ datastruct/main/java/lou/alex/train/datastruct/LinkedListNode.java
@@ -31,4 +31,24 @@ public class LinkedListNode<E> {
     public void setData(E data) {
         this.data = data;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        LinkedListNode that = (LinkedListNode) o;
+
+        if (data != null ? !data.equals(that.data) : that.data != null) return false;
+        if (next != null ? !next.equals(that.next) : that.next != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = next != null ? next.hashCode() : 0;
+        result = 31 * result + (data != null ? data.hashCode() : 0);
+        return result;
+    }
 }
