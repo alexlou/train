bc7cc6ef598075755b969e79750f9c959309203d
alexlou <chunwei.lou@gmail.com>
2014-10-30 22:45:39 +0800

    constructor for LinkedListNode

--- datastruct/main/java/lou/alex/train/datastruct/LinkedListNode.java
+++ datastruct/main/java/lou/alex/train/datastruct/LinkedListNode.java
@@ -3,5 +3,17 @@ package lou.alex.train.datastruct;
 /**
  * Created by alex on 30/10/14.
  */
-public class LinkedListNode {
+public class LinkedListNode<E> {
+    private LinkedListNode<E> next;
+    private E data;
+
+    public LinkedListNode(LinkedListNode<E> next, E data) {
+        this.next = next;
+        this.data = data;
+    }
+
+    public LinkedListNode(E data) {
+
+        this.data = data;
+    }
 }
